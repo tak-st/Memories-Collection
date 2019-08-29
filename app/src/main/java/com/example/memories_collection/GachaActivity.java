f8fb4dfb37563879b27a14e4e3d31ccc5d3686fc
Takuya Shintani <2180264@ecccomp.com>
2019-08-29 16:05:13 +0900

    GachaActivityの作成
    Signed-off-by: Takuya Shintani <2180264@ecccomp.com>

---
+++ app/src/main/java/com/example/memories_collection/GachaActivity.java
@@ -0,0 +1,14 @@
+package com.example.memories_collection;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GachaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gacha);
+    }
+}
