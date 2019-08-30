24702b4f6f36e4b30c7961db3e7b154a2204f53c
Takuya Shintani <2180264@ecccomp.com>
2019-08-30 16:50:45 +0900

    画面をタップしたらレア度が出るように設定
    Signed-off-by: Takuya Shintani <2180264@ecccomp.com>

--- app/src/main/java/com/example/memories_collection/GachaActivity.java
+++ app/src/main/java/com/example/memories_collection/GachaActivity.java
@@ -1,14 +1,41 @@
 package com.example.memories_collection;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GachaActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class GachaActivity extends AppCompatActivity {
+    int runflag = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gacha);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Random random = new Random();
+        int randomValue = random.nextInt(100);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (randomValue < 90) {
+            builder.setMessage("N");
+        } else if (randomValue < 99) {
+            builder.setMessage("R");
+        } else {
+            builder.setMessage("SR");
+        }
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.show();
+        return true;
+        //return super.onTouchEvent(event);
     }
 }
