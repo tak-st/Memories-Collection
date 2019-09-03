ff3a1ff26812e64c0e174db6c752260807ad0109
Takuya Shintani <2180264@ecccomp.com>
2019-09-04 00:22:54 +0900

    ガチャ画面のレイアウトを変更、いろんなサイズに対応（したはず）。

--- app/src/main/java/com/example/memories_collection/GachaActivity.java
+++ app/src/main/java/com/example/memories_collection/GachaActivity.java
@@ -1,8 +1,12 @@
 package com.example.memories_collection;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,13 +16,33 @@ import java.util.Random;
 
 public class GachaActivity extends AppCompatActivity {
 
-    int coin = 1000;
+    private static final String TAG = "GachaActivity";
+    private static final String PREF_FILE_NAME = "com.example.memories_collection.PREF_FILE_NAME";
+
+    private int coin = 1000;
     private int power = 1;
+    private double newitem = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gacha);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        SharedPreferences sharedPref = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+
+        // 値の取得
+        int intVal = sharedPref.getInt("UNLOCK_ITEM", 0);
+        if (intVal < 6) {
+            newitem = 2 / (intVal + 2);
+        } else {
+            newitem = 2 / (Math.pow((intVal - 5), 2) + 7);
+        }
+        TextView probt = findViewById(R.id.textView2);
+        probt.setText(String.format("%.1f%%", newitem * 100));
+        // 値の設定 (起動する毎に値が +1 されます)
+/*        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("MY_KEY", intVal + 1);
+        editor.apply();*/
         TextView tv = findViewById(R.id.textView);
         tv.setText(String.valueOf(coin));
         findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
@@ -57,7 +81,7 @@ public class GachaActivity extends AppCompatActivity {
 
     private void gachaClickEvent(View v) {
         Random random = new Random();
-        int randomValue = random.nextInt(100);
+        int randomValue = random.nextInt(1000);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         if (coin < power) {
             builder.setMessage("コインが足りません。");
