01af7d03ab0af45894e7f5044cf7177a7b0e2679
2180081 <wade.wr1eve@docomo.ne.jp>
2019-09-03 16:11:33 +0900

    GPS機能を追加

--- app/src/main/java/com/example/memories_collection/MainActivity.java
+++ app/src/main/java/com/example/memories_collection/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.memories_collection;
 
 import androidx.fragment.app.FragmentActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.annotation.NonNull;
+
+import android.location.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,9 +21,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdate;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+import android.Manifest;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, LocationListener {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    double late, lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +42,120 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        int percent = 0;
+
+        //int percent = 0;
         ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
         //bar.setMax(100);
         //bar.setProgress(percent,true);
         //bar.setMin(0);
         findViewById(R.id.imageButton).setOnClickListener(this);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},
+                    1000);
+        } else {
+            locationStart();
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 50, this);
+
+        }
     }
 
+    private void locationStart() {
+
+        // LocationManager インスタンス生成
+        locationManager =
+                (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled");
+        } else {
+            // GPSを設定するように促す
+            Intent settingsIntent =
+                    new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("debug", "not gpsEnable, startActivity");
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+
+            Log.d("debug", "checkSelfPermission false");
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 50, this);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug", "checkSelfPermission true");
+
+                locationStart();
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        /* API 29以降非推奨
+
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.d("debug", "LocationProvider.AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+        */
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        late = location.getLatitude();
+
+        lon = location.getLongitude();
+        LatLng ecc = new LatLng(late, lon);
+        mMap.addMarker(new MarkerOptions().position(ecc).title("Marker"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ecc));
+        CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(
+                new LatLng(late, lon), 12);
+
+        mMap.moveCamera(cUpdate);
+
+    }
     @Override
     public void onClick(View view) {
         if (view != null) {
             switch (view.getId()) {
                 case R.id.imageButton:
-                    //動作確認用、その打ち消す(マーカーを和歌山湾辺りに一つ増やすコード)
+                    //動作確認用、そのうち消す(マーカーを和歌山湾辺りに一つ増やすコード)
                     mMap.addMarker(new MarkerOptions().position(new LatLng(34, 135)).title("Markerrrrrrrrrrrrrrrr"));
                     //  設定画面を開く処理
+                    //ナビゲーションドロワー
 
                     break;
 
@@ -65,16 +179,26 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        onLocationChanged(location);
         // Add a marker in ecc and move the camera
-        LatLng ecc = new LatLng(34.706463, 135.503209);
-        mMap.addMarker(new MarkerOptions().position(ecc).title("Marker in ecc"));
+        LatLng ecc = new LatLng(late, lon);
+        mMap.addMarker(new MarkerOptions().position(ecc).title("Marker"));
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ecc));
         CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(
-                new LatLng(34.706463, 135.503209), 12);
+                new LatLng(late, lon), 12);
 
         mMap.moveCamera(cUpdate);
     }
 
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
