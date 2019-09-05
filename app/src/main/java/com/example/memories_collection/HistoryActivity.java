08ad7df7e2123dad124c11f3bc7b88b9ef6733d5
Tomoya Shirotsubaki <tmy8567@gmail.com>
2019-09-05 14:51:21 +0900

    マーカー画像の変更

--- app/src/main/java/com/example/memories_collection/HistoryActivity.java
+++ app/src/main/java/com/example/memories_collection/HistoryActivity.java
@@ -1,57 +1,38 @@
 package com.example.memories_collection;
 
-
 import androidx.fragment.app.FragmentActivity;
-
-
 
 import android.os.Bundle;
 
-
-
 import com.google.android.gms.maps.CameraUpdateFactory;
-
 import com.google.android.gms.maps.GoogleMap;
-
 import com.google.android.gms.maps.OnMapReadyCallback;
-
 import com.google.android.gms.maps.SupportMapFragment;
-
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import com.google.android.gms.maps.CameraUpdate;
-
 
 public class HistoryActivity extends FragmentActivity implements OnMapReadyCallback {
 
-
-
     private GoogleMap mMap;
-
+    private LatLng latlng;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main1);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(R.layout.activity_history);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-
                 .findFragmentById(R.id.map);
-
         mapFragment.getMapAsync(this);
-
     }
 
 
     /**
-
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -59,30 +40,76 @@ public class HistoryActivity extends FragmentActivity implements OnMapReadyCallb
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
-
      */
-
     @Override
-
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
 
 
-        // Add a marker in ecc and move the camera
+        double latitude = 35.68;
+        double longitude = 139.76;
 
-        LatLng ecc = new LatLng(34.706463, 135.503209);
+        latlng = new LatLng(latitude, longitude);
 
-        mMap.addMarker(new MarkerOptions().position(ecc).title("Marker in ecc"));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ecc));
-
-        CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(
-
-                new LatLng(34.706463, 135.503209), 12);
-
-        mMap.moveCamera(cUpdate);
+        setIcon(latitude, longitude);
 
     }
 
+    private void setMarker(double latitude, double longitude) {
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latlng);
+        markerOptions.title("Marker");
+        mMap.addMarker(markerOptions);
+
+        // ズーム
+        zoomMap(latitude, longitude);
+
+    }
+
+    private void setIcon(double latitude, double longitude) {
+
+        BitmapDescriptor descriptor =
+                BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_round);
+
+
+        GroundOverlayOptions overlayOptions = new GroundOverlayOptions();
+        overlayOptions.image(descriptor);
+
+
+        overlayOptions.anchor(0.5f, 0.5f);
+
+
+        overlayOptions.position(latlng, 300f, 300f);
+
+        GroundOverlay overlay = mMap.addGroundOverlay(overlayOptions);
+
+        zoomMap(latitude, longitude);
+
+
+        overlay.setTransparency(0.0F);
+    }
+
+    private void zoomMap(double latitude, double longitude) {
+
+        double south = latitude * (1 - 0.00005);
+        double west = longitude * (1 - 0.00005);
+        double north = latitude * (1 + 0.00005);
+        double east = longitude * (1 + 0.00005);
+
+
+        LatLngBounds bounds = LatLngBounds.builder()
+                .include(new LatLng(south, west))
+                .include(new LatLng(north, east))
+                .build();
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+
+
+        mMap.moveCamera(CameraUpdateFactory.
+                newLatLngBounds(bounds, width, height, 0));
+
+    }
 }
